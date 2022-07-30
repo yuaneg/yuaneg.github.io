6e38cc27a2c6cdbb3ad0aa337ee281123f9e29a4
袁恩光 <1534915553@qq.com>
2022-07-30 16:27:37 +0800

    change

--- ipchange.sh
+++ ipchange.sh
@@ -1,7 +1,7 @@
 pushd ~/yuaneg.github.io
 git pull
 ipold=`grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}' index.html`
-ip=`curl ifconfig.me`
+ip=`curl cip.cc | awk 'NR==1{print $3}'`
 if [ "$ip" != "$ipold" ];then
   sed -i s/$ipold/$ip/g index.html
   git add .
